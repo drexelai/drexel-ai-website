7ec49f5eb4b0dfd10f3287f79e0d884d7af1e94d
Alex Knyazkov <alex@knyazkov.com>
2023-10-18 16:09:17 -0400

    chore: change theme

--- src/components/common/PillButton.tsx
+++ src/components/common/PillButton.tsx
@@ -9,13 +9,13 @@ export const PillButton = ({ children, primaryColor, ...attributes }: PillButton
   return (
     <button
       className={`${
-        primaryColor ? 'before:bg-primary' : 'before:bg-primary/10 dark:before:border-gray-700 dark:before:bg-gray-800'
+        primaryColor
+          ? 'before:bg-primary'
+          : 'before:bg-secondary/30 dark:before:border-gray-700 dark:before:bg-gray-800'
       } relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max`}
       {...attributes}
     >
-      <span
-        className={`relative text-base font-semibold ${primaryColor ? 'text-white' : 'text-primary dark:text-white'}`}
-      >
+      <span className={`relative text-base font-semibold text-black ${!primaryColor && 'dark:text-white'}`}>
         {children}
       </span>
     </button>
