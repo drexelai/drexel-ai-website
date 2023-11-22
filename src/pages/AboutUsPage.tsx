c766166117a444338e639d3194034df27719d318
Arijit Chakma <ac4393@drexel.edu>
2023-11-21 22:58:30 -0500

    fixed the publication title's font color and added contact us info

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -9,15 +9,15 @@ export const AboutUsPage = () => {
       <Section>
           <h2 className="text-2xl font-bold text-gray-700 dark:text-white md:text-4xl">Mission</h2>
           <br/>
-          <p className="text-gray-600 dark:text-gray-300">{clubConfig.main.aboutUS.mission}</p>
+          <p className="text-gray-600 dark:text-gray-300 text-md lg:text-lg">{clubConfig.main.aboutUS.mission}</p>
 
       </Section>
       <Section>
-        <h2 className="text-2xl font-bold text-gray-700 dark:text-white md:text-4xl">What We Do</h2>
+        <h2 className="text-2xl font-bold text-gray-700 dark:text-white md:text-4xl pb-8">What We Do</h2>
         <br/>
         <ul className="space-y-8">
           {clubConfig.main.aboutUS.activities.map((activity) => (
-            <li key={activity.id} className="text-gray-600 dark:text-gray-300">
+            <li key={activity.id} className="text-gray-600 dark:text-gray-300 text-md lg:text-lg">
               <p className="text-gray-700 dark:text-white font-bold inline-block">{activity.title}</p>:{' '}
               {activity.description}
             </li>
@@ -28,7 +28,7 @@ export const AboutUsPage = () => {
         <h2 className="text-2xl font-bold text-gray-700 dark:text-white md:text-4xl">Contact Us</h2>
         <br/>
         {/* <p className="text-gray-600 dark:text-gray-300">Phone: {clubConfig.main.phone}</p> */}
-        <p className="text-gray-600 dark:text-gray-300"><a href="https://dragonlink.drexel.edu/organization/drexelai">President's Email</a></p>
+        <p className="text-gray-600 dark:text-gray-300"><a href="https://dragonlink.drexel.edu/organization/drexelai">az548@drexel.edu</a></p>
       </Section>
     </PageContainer>
   );
--- src/pages/ResearchPage.tsx
+++ src/pages/ResearchPage.tsx
@@ -19,25 +19,25 @@ export const ResearchPage = () => {
               <br/>
               <ul>
               <li>
-                <h3 className="font-bold"><a href="https://www.sciencedirect.com/science/article/pii/S2667318522000150">Recent advances and application of generative adversarial networks in drug discovery, development, and targeting</a></h3>
-                <p className="lg:mx-auto lg:w-6/12 text-gray-600 dark:text-gray-300">
+                <h3 className="font-bold text-md lg:text-lg dark:text-gray-100 pb-4"><a href="https://www.sciencedirect.com/science/article/pii/S2667318522000150">Recent advances and application of generative adversarial networks in drug discovery, development, and targeting</a></h3>
+                <p className="lg:mx-auto text-sm lg:text-md lg:w-6/12 text-gray-600 dark:text-gray-200">
                 Tripathi, S., Augustin, A. I., Sukumaran, R., Dheer, S., & Kim, E. (2022). HematoNet: expert level classification of bone marrow cytology morphology in hematological malignancy with deep learning. Artificial Intelligence in the Life Sciences, 2, 100043.
               </p></li><br/>
                 <li>
-                <h3 className="font-bold"><a href="https://www.sciencedirect.com/science/article/pii/S235291482200199X">RadGenNets: Deep learning-based radiogenomics model for gene mutation prediction in lung cancer</a></h3>
-                <p className="lg:mx-auto lg:w-6/12 text-gray-600 dark:text-gray-300">
+                <h3 className="font-bold text-md lg:text-lg dark:text-gray-100 pb-4"><a href="https://www.sciencedirect.com/science/article/pii/S235291482200199X">RadGenNets: Deep learning-based radiogenomics model for gene mutation prediction in lung cancer</a></h3>
+                <p className="lg:mx-auto text-sm lg:text-md lg:w-6/12 text-gray-600 dark:text-gray-200">
                 Tripathi, S., Moyer, E. J., Augustin, A. I., Zavalny, A., Dheer, S., Sukumaran, R., ... & Kim, E. (2022). Radgennets: Deep learning-based radiogenomics model for gene mutation prediction in lung cancer. Informatics in Medicine Unlocked, 33, 101062.
               </p></li>
               <br/>
               <li>
-                <h3 className="font-bold"><a href="https://ieeexplore.ieee.org/abstract/document/9533483">Towards Searching Efficient and Accurate Neural Network Architectures in Binary Classification Problems</a></h3>
-                <p className="lg:mx-auto lg:w-6/12 text-gray-600 dark:text-gray-300">
+                <h3 className="font-bold text-md lg:text-lg dark:text-gray-100 pb-4"><a href="https://ieeexplore.ieee.org/abstract/document/9533483">Towards Searching Efficient and Accurate Neural Network Architectures in Binary Classification Problems</a></h3>
+                <p className="lg:mx-auto text-sm lg:text-md lg:w-6/12 text-gray-600 dark:text-gray-200">
                 Alparslan, Y., Moyer, E. J., Isozaki, I. M., Schwartz, D., Dunlop, A., Dave, S., & Kim, E. (2021, July). Towards searching efficient and accurate neural network architectures in binary classification problems. In 2021 International Joint Conference on Neural Networks (IJCNN) (pp. 1-8). IEEE.
               </p></li>
               <br/>
               <li>
-                <h3 className="font-bold"><a href="https://arxiv.org/abs/2103.01121">Extreme volatility prediction in stock market: When gamestop meets long short-term memory networks</a></h3>
-                <p className="lg:mx-auto lg:w-6/12 text-gray-600 dark:text-gray-300">
+                <h3 className="font-bold text-md lg:text-lg dark:text-gray-100 pb-4"><a href="https://arxiv.org/abs/2103.01121">Extreme volatility prediction in stock market: When gamestop meets long short-term memory networks</a></h3>
+                <p className="lg:mx-auto text-sm lg:text-md lg:w-6/12 text-gray-600 dark:text-gray-200">
                 Alparslan, Yigit, and Edward Kim. "Extreme volatility prediction in stock market: When gamestop meets long short-term memory networks." arXiv preprint arXiv:2103.01121 (2021).
               </p></li>
 
